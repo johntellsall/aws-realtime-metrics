17b85f1ede0d4ae0d05570ea9e0cb2e463b70a3e
John Mitchell <john@johntellsall.com>
2016-08-07 20:31:51 -0700

    zap unused

--- app.py
+++ app.py
@@ -26,8 +26,6 @@ def background_thread():
         time_str = datetime.datetime.now().strftime('%H:%M:%S')
         stats = redis_store.hgetall('stats')
         response = {'stats': stats, 'time': time_str}
-    #         print('response: %s', response)
-
         socketio.emit('my response', response, namespace='/test')
 
 
@@ -43,52 +41,12 @@ def test_message(message):
          {'data': message['data'], 'count': session['receive_count']})
 
 
-@socketio.on('join', namespace='/test')
-def join(message):
-    join_room(message['room'])
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my response',
-         {'data': 'In rooms: ' + ', '.join(rooms()),
-          'count': session['receive_count']})
-
-
-@socketio.on('leave', namespace='/test')
-def leave(message):
-    leave_room(message['room'])
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my response',
-         {'data': 'In rooms: ' + ', '.join(rooms()),
-          'count': session['receive_count']})
-
-
-@socketio.on('close room', namespace='/test')
-def close(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my response', {'data': 'Room ' + message['room'] + ' is closing.',
-                         'count': session['receive_count']},
-         room=message['room'])
-    close_room(message['room'])
-
-
-@socketio.on('disconnect request', namespace='/test')
-def disconnect_request():
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my response',
-         {'data': 'Disconnected!', 'count': session['receive_count']})
-    disconnect()
-
-
 @socketio.on('connect', namespace='/test')
 def test_connect():
     global thread
     if thread is None:
         thread = socketio.start_background_task(target=background_thread)
     emit('my response', {'data': 'Connected', 'count': 0})
-
-
-@socketio.on('disconnect', namespace='/test')
-def test_disconnect():
-    print('Client disconnected', request.sid)
 
 
 if __name__ == '__main__':
