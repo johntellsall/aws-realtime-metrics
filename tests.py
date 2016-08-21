d385e628fad7e0b4f1fcc2b54f03dbbb816b754a
John Mitchell <john@johntellsall.com>
2016-08-21 15:20:46 -0700

    add API test; add "fast mode" which skips API tests

--- tests.py
+++ tests.py
@@ -1,8 +1,30 @@
+'''
+tests.py -- test AWS Realtime Mettrics funcs and APIs
+
+USAGE
+py.test tests.py
+
+- fast mode:
+SKIP_API=1 py.test tests.py
+'''
+
+import datetime
 import json
+import logging
+import os
 
 import boto3
+import pytest
 from moto import mock_s3, mock_sns, mock_sqs
 
+
+SKIP_API = os.environ.has_key('SKIP_API')
+logger = logging.getLogger(__name__)
+
+
+def format_unique_name(prefix):
+    return '{}-{}'.format(
+        prefix, datetime.datetime.now().strftime("%H%M%S-%f"))
 
 @mock_s3
 def test_boto3():
@@ -26,13 +48,25 @@ def test_sns():
     assert len(response['MessageId'])
 
 
-@mock_sqs
-def test_sqs():
-    sqs = boto3.resource('sqs', 'us-east-1')
-    queue = sqs.create_queue(QueueName='votes')
+def _test_sqs(queuename):
+    sqs = boto3.resource('sqs')
+    queue = sqs.create_queue(QueueName=queuename)
 
     queue.send_message(MessageBody=json.dumps({'beer': 'tasty'}))
 
     messages = queue.receive_messages()
-    assert len(messages)
+    assert len(messages) == 1
     assert messages[0].body == '{"beer": "tasty"}'
+
+@mock_sqs
+def test_sqs_mock():
+    _test_sqs('test')
+
+@pytest.mark.skipif('SKIP_API')
+def test_sqs():
+    qname = format_unique_name('test-sqs')
+    try:
+        _test_sqs(qname)
+    finally:
+        sqs = boto3.resource('sqs')
+        sqs.get_queue_by_name(QueueName=qname).delete()
